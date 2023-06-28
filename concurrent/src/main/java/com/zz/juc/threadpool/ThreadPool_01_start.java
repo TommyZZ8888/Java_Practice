2c4c852695b3c8064bb8f02ee33b50938eaaf772
TommyZZ8888 <1950442495@qq.com>
2023-06-28 17:35:36 +0800

    recommit

--- concurrent/src/main/java/com/zz/juc/threadpool/ThreadPool_01_start.java
+++ concurrent/src/main/java/com/zz/juc/threadpool/ThreadPool_01_start.java
@@ -13,10 +13,11 @@ public class ThreadPool_01_start {
     public static void main(String[] args) {
         System.out.println(Runtime.getRuntime().availableProcessors());
         int queueSize = 2000;
-        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(10,
-                50,
-                300L, TimeUnit.SECONDS,
-                new LinkedBlockingQueue<>(queueSize),
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
+                10,// int corePoolSize, 核心线程池大小(候客区窗口10个)
+                50,// int maximumPoolSize, 最大核心线程池大小(总共50个窗口)
+                300L, TimeUnit.SECONDS,// long keepAliveTime, 超时3秒没有人调用就会释，放关闭窗口
+                new LinkedBlockingQueue<>(queueSize),// 阻塞队列(候客区最多2000人)
                 MyThreadFactory.create("start")
                 );
 
