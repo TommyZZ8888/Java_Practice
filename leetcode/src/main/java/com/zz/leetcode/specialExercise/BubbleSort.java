725839bf223c6986ef63c242993572ada7ed9b26
TommyZZ8888 <1950442495@qq.com>
2023-07-26 15:03:23 +0800

    test-test

--- leetcode/src/main/java/com/zz/leetcode/specialExercise/BubbleSort.java
+++ leetcode/src/main/java/com/zz/leetcode/specialExercise/BubbleSort.java
@@ -20,9 +20,6 @@ public class BubbleSort {
         int len = nums.length;
 
         for (int i = 0; i < len - 1; i++) {
-
-
-
             for (int j = 0; j < len - i - 1; j++) {
                 if (nums[j] > nums[j + 1]) {
                     int temp = nums[j];
